d2633759fdc1359eb88c2664150cc64487734c51
Lalves01 <50640331+rfaavs@users.noreply.github.com>
2019-10-23 01:32:20 -0200

    teste1

---
+++ 04media1.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a quarta nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+soma = nota1 + nota2 + nota3 + nota4 
+media  = soma / 4
+
+print("A media do aluno é: ",media)
---
+++ 05metrostocentimetros.py
@@ -0,0 +1,4 @@
+metros = float(input("Digite a metragem a sere convertida em centimetro: "))
+centimetros = metros * 1000
+
+print("O resultado da conversao é: ",centimetros)
