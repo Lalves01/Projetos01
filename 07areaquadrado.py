ecc6cde8f65a36a746ed2dde5742da0ff578869a
Lalves01 <50640331+rfaavs@users.noreply.github.com>
2019-10-24 19:32:00 -0200

    07e08

---
+++ 07areaquadrado.py
@@ -0,0 +1,4 @@
+lado = float(input("Informe o tamanho de um dos lados do quadrado: "))
+area = lado * lado
+dobro = area + area
+print("O dobro da area do quadrado é: ", dobro)
---
+++ 08salario.py
@@ -0,0 +1,8 @@
+ganho = float(input("Informe quanto você ganha por hora trabalhado: "))
+horasd = int(input("Agora informe quantas horas você trabalha por dia: "))
+dias = int(input("Agora informe quantos dias voce trabalha por mes: "))
+sdia = ganho * horasd
+smensal = sdia * dias
+print("Seu ganho diario é: ", sdia)
+print("Já seu salario mensal é: ", smensal)
+
