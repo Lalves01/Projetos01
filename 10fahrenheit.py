9ae6ba01a7df6be1426cae5231a7ade37c457638
Lalves01 <50640331+rfaavs@users.noreply.github.com>
2019-10-25 18:48:21 -0200

    10.11.12

---
+++ 10fahrenheit.py
@@ -0,0 +1,3 @@
+c = int(input("Digite a temperatura em graus celsius: "))
+f = (c * (9/5) + 32)
+print("A temperatura em graus fahrenheit é: ", f)
---
+++ 11intprodfloat.py
@@ -0,0 +1,12 @@
+n1 = int(input("Digite o primeiro número inteiro: "))
+n2 = int(input("Digite o segundo número inteiro: "))
+n3 = float(input("Agora digite um número real: "))
+
+a = ((n1 + n1) + (n2 / 2))
+print("Questão a: ", a)
+
+b = ((n1 * 3) + n3)
+print("Questão b: ", b)
+
+c = (n3 ** 3)
+print("Questão c: ", c)
